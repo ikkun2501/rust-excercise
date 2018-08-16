42da51666572bfc19465206d08f6501a6289d16e
ikkun2501 <ikkun2501@gmail.com>
2018-08-16 16:50:08 +0900

    add lifetime_struct

---
+++ programming-rust/9/lifetime_struct/target/debug/deps/lifetime_struct-6e84cc977b2b73fb.d
@@ -0,0 +1,5 @@
+/Users/hal/IdeaProjects/rust-excercise/programming-rust/9/lifetime_struct/target/debug/deps/lifetime_struct-6e84cc977b2b73fb: src/main.rs
+
+/Users/hal/IdeaProjects/rust-excercise/programming-rust/9/lifetime_struct/target/debug/deps/lifetime_struct-6e84cc977b2b73fb.d: src/main.rs
+
+src/main.rs:
---
+++ programming-rust/9/lifetime_struct/target/debug/lifetime_struct-6e84cc977b2b73fb.d
@@ -0,0 +1,1 @@
+/Users/hal/IdeaProjects/rust-excercise/programming-rust/9/lifetime_struct/target/debug/lifetime_struct-6e84cc977b2b73fb.dSYM: /Users/hal/IdeaProjects/rust-excercise/programming-rust/9/lifetime_struct/src/main.rs
